4bb880246bbe30cd0cc2c28e3f0effbf037814ea
Danny Vaca <danny270793@icloud.com>
2025-05-17 12:01:51 -0500

    rener in the dom asyncronously

--- src/my-react.ts
+++ src/my-react.ts
@@ -1,9 +1,16 @@
 export interface MyReactElement {
-  type: string
+  type: string | null
   props: {
     [key: string]: any
     children: MyReactElement[]
   }
+}
+
+export interface NextUnitOfWork extends MyReactElement {
+  dom: HTMLElement | Text | null
+  parent: NextUnitOfWork | null
+  sibling: NextUnitOfWork | null
+  child: NextUnitOfWork | null
 }
 
 export class MyReact {
@@ -31,34 +38,63 @@ export class MyReact {
       },
     }
   }
-  static render(element: MyReactElement, container: HTMLElement | Text) {
+  static createDom(fiber: MyReactElement): HTMLElement | Text {
     // create dom node
     const dom: HTMLElement | Text =
-      element.type == 'TEXT_ELEMENT'
+      fiber.type == 'TEXT_ELEMENT'
         ? document.createTextNode('')
-        : document.createElement(element.type)
+        : document.createElement(fiber.type || '')
 
     // assig props to node
     const isProperty = (key: string) => key !== 'children'
-    Object.keys(element.props)
+    Object.keys(fiber.props)
       .filter(isProperty)
       .forEach((name: string) => {
-        const prop: string = element.props[name]
+        const prop: string = fiber.props[name]
         const domAsAny: any = dom as any
         domAsAny[name] = prop
       })
 
-    // render childrens
-    element.props.children.forEach((child: MyReactElement) =>
-      MyReact.render(child, dom),
-    )
+    return dom
+  }
+  static commitRoot() {
+    MyReact.commitWork(MyReact.wipRoot?.child || null)
+    MyReact.wipRoot = null
+  }
+  static commitWork(fiber: NextUnitOfWork | null) {
+    if (!fiber) {
+      return
+    }
+    if (!fiber.parent?.dom) {
+      throw new Error('No parent dom found')
+    }
+    const domParent: HTMLElement | Text = fiber.parent.dom
+    if (!fiber.dom) {
+      throw new Error('No dom found')
+    }
+    domParent.appendChild(fiber.dom)
+    MyReact.commitWork(fiber.child)
+    MyReact.commitWork(fiber.sibling)
+  }
+  static render(element: MyReactElement, container: HTMLElement | Text) {
+    MyReact.wipRoot = {
+      dom: container,
+      type: null,
+      parent: null,
+      sibling: null,
+      child: null,
+      props: {
+        children: [element],
+      },
+    }
 
-    // append to container
-    container.appendChild(dom)
+    // define next unit of work
+    MyReact.nextUnitOfWork = MyReact.wipRoot
   }
 
   // create microtasks to not interrupt browser
-  static nextUnitOfWork: any = null
+  static nextUnitOfWork: NextUnitOfWork | null = null
+  static wipRoot: NextUnitOfWork | null = null
   static workLoop(deadline: IdleDeadline) {
     let shouldYield = false
     while (MyReact.nextUnitOfWork && !shouldYield) {
@@ -67,10 +103,59 @@ export class MyReact {
       // check if we have time to do more work
       shouldYield = deadline.timeRemaining() < 1
     }
+
+    if (!MyReact.nextUnitOfWork && MyReact.wipRoot) {
+      MyReact.commitRoot()
+    }
+
     // if we have no more work, schedule the next unit of work
     requestIdleCallback(MyReact.workLoop)
   }
-  static performUnitOfWork(nextUnitOfWork: any): any {}
+  static performUnitOfWork(nextUnitOfWork: NextUnitOfWork): any {
+    // add dom node
+    if (!nextUnitOfWork.dom) {
+      nextUnitOfWork.dom = MyReact.createDom(nextUnitOfWork)
+    }
+
+    // create new fibers
+    const elements: MyReactElement[] = nextUnitOfWork.props.children
+    let index: number = 0
+    let prevSibling: NextUnitOfWork | null = null
+    while (index < elements.length) {
+      const element: MyReactElement = elements[index]
+      const newFiber: NextUnitOfWork = {
+        type: element.type,
+        props: element.props,
+        parent: nextUnitOfWork,
+        dom: null,
+        sibling: null,
+        child: null,
+      }
+
+      if (index === 0) {
+        nextUnitOfWork.child = newFiber
+      } else {
+        if (!prevSibling) {
+          throw new Error('No previous sibling found')
+        }
+        prevSibling.sibling = newFiber
+      }
+      prevSibling = newFiber
+      index++
+    }
+
+    // return next unit of work
+    if (nextUnitOfWork.child) {
+      return nextUnitOfWork.child
+    }
+    let nextFiber: NextUnitOfWork | null = nextUnitOfWork
+    while (nextFiber) {
+      if (nextFiber.sibling) {
+        return nextFiber.sibling
+      }
+      nextFiber = nextFiber.parent
+    }
+  }
 }
 
 requestIdleCallback(MyReact.workLoop)
