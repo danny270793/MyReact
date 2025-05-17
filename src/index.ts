f80d8c5791b1674ad859de9f9cd51446dd3a4abf
Danny Vaca <danny270793@icloud.com>
2025-05-17 10:46:03 -0500

    use create element function

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { MyReact } from './libraries/my-react'
+
+const element = MyReact.createElement(
+  'div',
+  { id: 'foo' },
+  MyReact.createElement('a', null, 'bar'),
+  MyReact.createElement('b'),
+)
+const container = document.getElementById('root')
