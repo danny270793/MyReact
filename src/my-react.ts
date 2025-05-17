457a5ba59208c5f70bf7adc8e37b37c7d9c40e42
Danny Vaca <danny270793@icloud.com>
2025-05-17 11:04:03 -0500

    my react library

---
+++ src/my-react.ts
@@ -0,0 +1,31 @@
+export interface MyReactElement {
+    type: string
+    props: {
+        [key: string]: any
+        children: MyReactElement[]
+    }
+}
+
+export class MyReact {
+  static createElement(type: string, props: {[key: string]: any}|null = null, ...children: MyReactElement[]): MyReactElement {
+    return {
+      type,
+      props: {
+        ...props,
+        children: children.map((child: MyReactElement) =>
+          typeof child === 'object' ? child : MyReact.createTextElement(child),
+        ),
+      },
+    }
+  }
+  static createTextElement(text: string) {
+    return {
+      type: 'TEXT_ELEMENT',
+      props: {
+        nodeValue: text,
+        children: [],
+      },
+    }
+  }
+  static render(element: MyReactElement, container: HTMLElement) {}
+}
