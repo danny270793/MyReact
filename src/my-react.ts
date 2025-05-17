2dbcd6902e8836d6d784e84fc1da8b243234538e
Danny Vaca <danny270793@icloud.com>
2025-05-17 11:24:09 -0500

    syncronous render

--- src/my-react.ts
+++ src/my-react.ts
@@ -31,8 +31,29 @@ export class MyReact {
       },
     }
   }
-  static render(element: MyReactElement, container: HTMLElement) {
-    const dom: HTMLElement = document.createElement(element.type)
+  static render(element: MyReactElement, container: HTMLElement | Text) {
+    // create dom node
+    const dom: HTMLElement | Text =
+      element.type == 'TEXT_ELEMENT'
+        ? document.createTextNode('')
+        : document.createElement(element.type)
+
+    // assig props to node
+    const isProperty = (key: string) => key !== 'children'
+    Object.keys(element.props)
+      .filter(isProperty)
+      .forEach((name: string) => {
+        const prop: string = element.props[name]
+        const domAsAny: any = dom as any
+        domAsAny[name] = prop
+      })
+
+    // render childrens
+    element.props.children.forEach((child: MyReactElement) =>
+      MyReact.render(child, dom),
+    )
+
+    // append to container
     container.appendChild(dom)
   }
 }
