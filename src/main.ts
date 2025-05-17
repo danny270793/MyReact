c5f59bf6623a3e4a8d1eeae20f1a0f56e1ac0de9
Danny Vaca <danny270793@icloud.com>
2025-05-17 11:09:01 -0500

    create dom

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
+import { MyReact, type MyReactElement } from './my-react'
+
 const element: HTMLElement | null = document.getElementById('app')
 if (!element) {
   throw new Error('No root element found')
 }
+
+const app: MyReactElement = MyReact.createElement(
+  'div',
+  { id: 'foo' },
+  MyReact.createElement('a', null, 'bar'),
+  MyReact.createElement('b'),
+)
+MyReact.render(app, element)
